5acd6430fef1254b8f4c82a95025470767a35b0c
Nuno <96051586+Estgv18755@users.noreply.github.com>
2022-07-01 14:11:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 //Criar o Server
 const express = require('express');
-
 const app = express();
-app.use(express.json());
-//const port = 4000;
-
-app.listen('port', process.env.PORT||4000)
-
-//Ligar à Base de Dados
+const middleware = require('./src/middleware');
+var bodyParser = require('body-parser');
 const bd = require('./src/config/basedados');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.set('port', process.env.PORT||4000)
+
+app.use(express.json());
 
 bd.authenticate()
 .then(console.log('Connection has been established successfully.'))
@@ -32,9 +35,6 @@ app.use((req, res, next) => {
 
 /* const indexmodels = require('./src/models/index'); */
 
-//Importar MiddleWare
-const middleware = require('./src/middleware');
-
 // Rotas
 const rota = require('./src/routes/RotasTestData');
 const rotasCentro = require('./src/routes/RotasCentro');
@@ -47,4 +47,8 @@ app.use('/utilizadores', rotasUtilizador);
 app.use('/salas', rotasSala); 
 app.use('/testdata', middleware.checkToken, rota);
 app.use('/reservas',middleware.checkToken, rotasReservas);  
+
+app.listen(app.get('port'),()=>{
+    console.log("Start server on port "+app.get('port'))
+})
 
